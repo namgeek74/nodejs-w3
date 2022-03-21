9cacc57e1193328e621c6513da39e43be2d772d7
namnd <namnguyennd7428@gmail.com>
2022-03-22 01:34:30 +0700

    http module

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var http = require('http');
 var dt = require('./date-module');
+var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.write(txt + "/n");
+    console.log(req.url);
     res.write("The date and time are currently: " + dt.myDateTime());
     res.end();
 }).listen(8080);
